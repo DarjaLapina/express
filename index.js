116afe9db5d04063252026acff33b614307da1a8
Satis <112948513+DarjaLapina@users.noreply.github.com>
2024-06-05 09:59:06 +0300

    Start use handlebars

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const { addListener } = require('nodemon')
-
+const exphbs = require('express-handlebars')
 const path = require('path')
+
 const app = express()
 
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname,'views', 'index.html'))
